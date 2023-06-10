9d16e333aeb9481686bc40701f9b5ecfc73d97a5
kuba1pie <kuba1pie@gmail.com>
2023-06-10 16:43:27 +0200

    feat: dummy fetch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,32 +5,22 @@ defineProps({
   msg: String,
 })
 
-const count = ref(0)
+let res = ref({})
+
+async function fetchData() {
+  const response = await fetch("https://dummyjson.com/products");
+  const jsonData = await response.json();
+  res = jsonData
+}
+
+fetchData()
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <li v-for="item in res.products">
+    {{ item.title }}
+  </li>
 </template>
 
 <style scoped>
