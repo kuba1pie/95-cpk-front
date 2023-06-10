09360b7c679eac9fa7b269a30633e20c28f86bcf
kuba1pie <kuba1pie@gmail.com>
2023-06-10 16:55:35 +0200

    feat: dummy fetch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,16 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-let res = ref({})
-
-async function fetchData() {
-  const response = await fetch("https://dummyjson.com/products");
-  const jsonData = await response.json();
-  res = jsonData
-}
-
-fetchData()
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-  <li v-for="item in res.products">
-    {{ item.title }}
-  </li>
+  <ul>
+    <li v-for="item in items.products" :key="item.id">{{ item.title }}</li>
+  </ul>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  const response = await fetch('https://dummyjson.com/products')
+  items.value = await response.json()
+})
+</script>
